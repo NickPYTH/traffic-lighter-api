ef96ea6d44b4291bb6f0de329c7e2f5891a572f6
Николай С <nicksrnk@gmail.com>
2025-12-12 23:43:37 +0500

    Добавил новые модели

--- backend/trafficLighter/api/models.py
+++ backend/trafficLighter/api/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    department = models.ForeignKey(Department, on_delete=models.DO_NOTHING)
+
