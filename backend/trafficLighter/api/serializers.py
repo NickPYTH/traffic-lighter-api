843b1cdfa1d5de36608a43638c114ba54acfe9de
Николай С <nicksrnk@gmail.com>
2025-12-14 00:00:52 +0500

    Добавлен сериализатор для Profile

--- backend/trafficLighter/api/serializers.py
+++ backend/trafficLighter/api/serializers.py
@@ -1,9 +1,24 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
-from .models import Department
+from .models import Department, Profile
 
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
 
 class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Department
         fields = ('id', 'name', 'description')
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(many=True)
+    department = DepartmentSerializer(many=True)
+
+    class Meta:
+        model = Profile
+        fields = ('id', 'user', 'department')
+
